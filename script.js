49de8691df4496c08cf1e84ac710e3f2ce9d6d61
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2023-10-07 10:05:38 -0700

    updating classes, js, and css

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-let text = document.getElementById('text');
+const text = document.getElementById('text');
+const bird1 = document.getElementById('bird1');
+const bird2 = document.getElementById('bird2');
+const forest = document.getElementById('forest');
+const btn = document.getElementById('btn');
+const rocks = document.getElementById('rocks');
+const header = document.getElementById('header');
 
 window.addEventListener('scroll', () => {
   let value = window.scrollY;
 
-  text.style.marginTop = value * 2.5 + 'px';
+  text.style.top = 50 + value * -0.5 + '%';
+  bird1.style.top = value * -1.5 + 'px';
+  bird1.style.left = value * 2 + 'px';
+  bird2.style.top = value * -1.5 + 'px';
+  bird2.style.left = value * -5 + 'px';
+  btn.style.marginTop = value * 1.5 + 'px';
+  rocks.style.top = value * -0.12 + 'px';
+  forest.style.top = value * 0.25 + 'px';
+  header.style.top = value * 0.5 + 'px';
 });
