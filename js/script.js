5ad65d4f464e6625e22545c677a7c731a0ff03fb
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2023-10-16 16:43:11 -0700

    added scrolltotop button and js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// js for the parallax background image
+// js for background images
 const text = document.getElementById('text');
 const bird1 = document.getElementById('bird1');
 const bird2 = document.getElementById('bird2');
@@ -6,6 +6,8 @@ const forest = document.getElementById('forest');
 const btn = document.getElementById('btn');
 const rocks = document.getElementById('rocks');
 const header = document.getElementById('header');
+// for scroll button
+const scrollToTopButton = document.getElementById('scrollToTopButton');
 
 window.addEventListener('scroll', () => {
   let value = window.scrollY;
@@ -38,3 +40,42 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     }
   });
 });
+
+/**
+ * when the user scrolls down 100px fro the top of the document, the button appears
+ */
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    scrollToTopButton.style.display = 'block';
+  } else {
+    scrollToTopButton.style.display = 'none';
+  }
+}
+
+// Smooth scrolling to the top when the button is clicked
+scrollToTopButton.addEventListener('click', () => {
+  scrollToTopSmoothly();
+});
+
+function scrollToTopSmoothly() {
+  const scrollToTop = () => {
+    const currentPosition =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentPosition > 0) {
+      // Calculate how far to scroll
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, currentPosition - currentPosition / 8);
+    }
+  };
+
+  scrollToTop();
+}
